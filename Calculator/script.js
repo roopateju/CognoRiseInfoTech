9cc556c7b8120a27e5dbb7290f557bfc9b3d517b
roopateju <167866093+roopateju@users.noreply.github.com>
2024-07-20 12:15:41 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,23 @@
+let display = document.getElementById('display');
+
+function appendNumber(num) {
+    display.value += num;
+}
+
+function appendOperator(operator) {
+    display.value += operator;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+    
